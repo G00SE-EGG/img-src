51353e61b222ec98e261066ed8cd4858a18141fb
g00se_egg <simuelhenderson@gmail.com>
2016-05-12 03:03:23 +0000

    updated app.js

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var express = require("express");
+var Search = require("bing.search");
 var app = express();
-
+var search = new Search('wzPhvJlN7jnEVXF+Nf5RPwFx6nydyOzIcqlOQCBJlHM');
 app.set('port', process.env.PORT || 3000);
-
+var testObj = {};
 app.get('/', function(req, res){
     res.status(200);
    res.send('<h1>Hello, World</h1>'); 
 });
 
 
-app.get('/api', function(req, res) {
+app.get('/search*', function(req, res) {
    res.status(200); 
-   res.send('<h1>API</h1>');
+   var srch = req.url.replace(/[\/search\/%20]/g, ' ');
+   res.send(srch);
+   search.images(srch, {top:5}, function(err, data){
+      if(err)console.error(err);
+      console.log(data);
+   });
+   
 });
 
 app.get('/recent', function(req, res) {
