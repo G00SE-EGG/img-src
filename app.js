467d43ffd79b5a231a0405d313d3f64379662cc9
g00se_egg <simuelhenderson@gmail.com>
2016-05-17 03:21:34 +0000

    updated app.js

--- app.js
+++ app.js
@@ -3,23 +3,46 @@ var Search = require("bing.search");
 var app = express();
 var search = new Search('wzPhvJlN7jnEVXF+Nf5RPwFx6nydyOzIcqlOQCBJlHM');
 app.set('port', process.env.PORT || 3000);
-var testObj = {};
+var testObj = [];
 app.get('/', function(req, res){
     res.status(200);
    res.send('<h1>Hello, World</h1>'); 
 });
 
+function imgQuery(url, alttext, thumbnail)
+{
+    this.url = url;
+    this.alttext = alttext;
+    this.thumbnail;
+}
 
 app.get('/search*', function(req, res) {
    res.status(200); 
-   var srch = req.url.replace(/[\/search\/%20]/g, ' ');
+   var srch = req.url.split('/')[2].split('?')[0].replace(/%20/g, ' ');//.replace(/[\/search\/%20]/g, ' ');
    //var offset = req.url.split('?');
    var offset = req.url.split('?')[1].split('=')[1].replace(/\&/g, '');
+   console.log(srch);
    search.images(srch, {top:offset}, function(err, data){
       if(err)console.error(err);
-      console.log(offset);
-      res.send(data);
+      
+      console.log(srch);
+      for(var i = 0; i < offset; i++)
+      {
+          testObj.push(new imgQuery(data[i]['url'], data[i]['title'], data[i]['thumbnail']['url'] ));
+      }
+      //for(var j = 0; j < offset; j++)
+      //{
+          res.send(testObj);
+      //}
+      srch = "";
+      for(var j = 0; j < offset; j++)
+      {
+          testObj[j].url = "";
+          testObj[j].alttext = "";
+          testObj[j].thumbnail = "";
+      }
    });
+   
    
 });
 
